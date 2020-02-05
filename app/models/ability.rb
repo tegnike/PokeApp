acd329b7b8bd3b53ca1dd1c178d6068f8ad94595
tegnike <user@mbp.local>
2020-02-05 14:08:06 +0100

    add Ability model

---
+++ app/models/ability.rb
@@ -0,0 +1,3 @@
+class Ability < ApplicationRecord
+  validates :ability, presence: true
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-
   let(:user) { create(:user) }
 
   describe "User validation" do
