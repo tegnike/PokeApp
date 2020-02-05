0f048a879a7a7830bccf21461ee62ecb921b6633
tegnike <user@mbp.local>
2020-02-05 16:57:32 +0100

    add Item model

---
+++ app/models/item.rb
@@ -0,0 +1,3 @@
+class Item < ApplicationRecord
+  validates :item, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_143241) do
+ActiveRecord::Schema.define(version: 2020_02_05_152629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "abilities", force: :cascade do |t|
     t.string "ability"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "item"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
