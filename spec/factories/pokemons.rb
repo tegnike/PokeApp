98d2d9e57c91df1a223d7be6d7d024168644b1fe
tegnike <user@mbp.local>
2020-03-18 10:37:28 +0100

    update my_pokemon model spec

--- spec/factories/pokemons.rb
+++ spec/factories/pokemons.rb
@@ -1,15 +1,17 @@
 FactoryBot.define do
   factory :pokemon do
-    number { 151 }
-    name { Faker::Games::Pokemon.name }
+    number { 25 }
+    name { "ピカチュウ" }
     type1 { 0 }
     type2 { 1 }
-    form { "a" }
+    form { "" }
     bs_h { 80 }
     bs_a { 80 }
     bs_b { 80 }
     bs_c { 80 }
     bs_d { 80 }
     bs_s { 80 }
+    form_name { "" }
+    full_name { "ピカチュウ" }
   end
 end
--- spec/forms/my_pokemon_form_spec.rb
+++ spec/forms/my_pokemon_form_spec.rb
@@ -1,11 +1,31 @@
 require "rails_helper"
 
 RSpec.describe MyPokemonForm, type: :model do
-  let(:my_pokemon) { create(:my_pokemon) }
-
+  before {
+    user = create(:user)
+    create(:pokemon, full_name: "テストポケモン")
+    create(:ability, ability: "テスト特性")
+    create(:item, item: "テストアイテム")
+    (1..4).each do |n|
+      create(:move, move: "テストわざ#{n}")
+    end
+    @my_pokemon_form = MyPokemonForm.new(
+      user_id: user.id,
+      name: "テストポケモン",
+      ability: "テスト特性",
+      status_up: "0",
+      status_down: "2",
+      item: "テストアイテム",
+      ev_h: 4, ev_a: 252, ev_b: 0, ev_c: 0, ev_d: 0, ev_s: 252,
+      iv_h: "31", iv_a: "31", iv_b: "31", iv_c: "", iv_d: "31", iv_s: "31",
+      move_1: "テストわざ1", move_2: "テストわざ2",
+      move_3: "テストわざ3", move_4: "テストわざ4",
+      role: "テスト役割"
+    )
+  }
   describe "MyPokemon validation" do
     it "shows my_pokemon is valid" do
-      expect(my_pokemon).to be_valid
+      expect(@my_pokemon_form).to be_valid
     end
   end
 
@@ -34,6 +54,43 @@ RSpec.describe MyPokemonForm, type: :model do
     end
     describe "characters" do
       it { should validate_length_of(:role).is_at_most(20) }
+    end
+  end
+
+  describe "save" do
+    it "shows save method true" do
+      expect(@my_pokemon_form.save).to be_truthy
+    end
+  end
+
+  describe "integer_string?(str)" do
+    it "shows correct result" do
+      @my_pokemon_form.save
+      my_pokemon = MyPokemon.find_by(role: "テスト役割")
+      expect(my_pokemon.iv_a).to eq "31"
+      expect(my_pokemon.iv_c).to eq nil
+    end
+  end
+
+  describe "get_status(status)" do
+    it "shows correct status" do
+      @my_pokemon_form.save
+      my_pokemon = MyPokemon.find_by(role: "テスト役割")
+      expect(my_pokemon.status_up).to eq "A"
+      expect(my_pokemon.status_down).to eq "C"
+    end
+  end
+
+  describe "calc_av(pokemon)" do
+    it "shows correct calculation" do
+      @my_pokemon_form.save
+      my_pokemon = MyPokemon.find_by(role: "テスト役割")
+      expect(my_pokemon.av_h).to eq 156
+      expect(my_pokemon.av_a).to eq 145
+      expect(my_pokemon.av_b).to eq 100
+      expect(my_pokemon.av_c).to eq nil
+      expect(my_pokemon.av_d).to eq 100
+      expect(my_pokemon.av_s).to eq 132
     end
   end
 end
